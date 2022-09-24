ec4843cd74d0ad013056a121150de5071504ccf4
TheVispan <theaveragesiren@gmail.com>
2022-09-25 02:17:51 +0500

    7.7 end

--- project/articles/admin.py
+++ project/articles/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Article
 
-admin.site.register(Article)
+from . import models
+
+class CommentInline(admin.TabularInline):
+    model = models.Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
